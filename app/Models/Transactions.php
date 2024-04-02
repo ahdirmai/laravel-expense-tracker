a7ad2f63825a3d3e92991f79431038f3dfea9f55
Ridha Fahmi J <61994966+EvolutionRF@users.noreply.github.com>
2024-04-03 04:05:16 +0800

    add relations on model categories and transaction

--- app/Models/Transactions.php
+++ app/Models/Transactions.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transactions extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+    protected $guarded = [];
+
+    public function category()
+    {
+        return $this->belongsTo(Categories::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
